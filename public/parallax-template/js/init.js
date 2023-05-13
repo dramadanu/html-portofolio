b691df1932159729846115a31023d0762db09df0
dramadanu <122292854+dramadanu@users.noreply.github.com>
2023-05-13 14:50:17 +0700

    Add files via upload

---
+++ public/parallax-template/js/init.js
@@ -0,0 +1,25 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+  // document.addEventListener('DOMContentLoaded', function() {
+  //   var elems = document.querySelectorAll('.slider');
+  //   var instances = M.Slider.init(elems, {
+  //     indicator: false,
+  //     height: 500,
+  //     transition: 600,
+  //     interval: 1800
+  //   });
+  // });
+
+  // // Or with jQuery
+
+  // $(document).ready(function(){
+  //   $('.slider').slider();
+  // });
